99ad1a33d7d49517bfd207c1a6e0cae36a650ee3
Tharanya06 <tharanyapushparaj34@gmail.com>
2025-08-29 23:26:50 +0530

    Update start-app.sh

--- start-app.sh
+++ start-app.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Starting app..."
-docker-compose up -d
-echo "App is running at http://localhost:5000"
 
+# Start Redis
+docker run -d --name redis_server --network app_network -v redis_data:/data --restart always redis:7
+
+# Start Flask app
+docker run -d --name flask_app --network app_network -p 5000:5000 --restart always -e REDIS_HOST=redis_server flask_app
+
+echo "App is running at http://localhost:5000"
